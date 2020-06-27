80a3b59816d1280c7e8b97534f19b97208ea6fc2
jeraldtsy <jerladtsy@gmail.com>
2020-06-27 13:55:11 +0800

    modifying lookout

---
+++ Components/AboutMe.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import profileImage from './img/DisplayPicture.jpg'
+import './AboutMe.css';
+
+return (
+	<Image src={profileImage} roundedCircle/>
+);
--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import profileImage from './img/DisplayPicture.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1> My React App </h1>
-      <h2> Author: Jerald</h2>
-      <img src = {profileImage} alt = "profile-image"/>
-      <p> Hello world!.</p>
+      <h1> Jerald </h1>
     </div>
   );
 }

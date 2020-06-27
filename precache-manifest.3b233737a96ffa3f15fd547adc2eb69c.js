f2abbf79958ef69cef1fb52462d329d42d7b80ae
jeraldtsy <jerladtsy@gmail.com>
2020-06-27 12:06:43 +0800

    Updates

---
+++ precache-manifest.3b233737a96ffa3f15fd547adc2eb69c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a7eb29b6c6d6da248af5cc137c7070f",
+    "url": "/jeraldtsy/index.html"
+  },
+  {
+    "revision": "92f1c7e8b3b34aae0108",
+    "url": "/jeraldtsy/static/css/main.5384a316.chunk.css"
+  },
+  {
+    "revision": "7f7506d485d96d436dd5",
+    "url": "/jeraldtsy/static/js/2.4cbe87e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jeraldtsy/static/js/2.4cbe87e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92f1c7e8b3b34aae0108",
+    "url": "/jeraldtsy/static/js/main.ae758c36.chunk.js"
+  },
+  {
+    "revision": "378eb9f62814c946a32e",
+    "url": "/jeraldtsy/static/js/runtime-main.12a95b19.js"
+  },
+  {
+    "revision": "3bc6d40b4a4a78eed96ffdb00a9d778b",
+    "url": "/jeraldtsy/static/media/DisplayPicture.3bc6d40b.jpg"
+  }
+]);

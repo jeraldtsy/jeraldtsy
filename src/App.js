35c6dd03dbafd92ad21422bb2b260cffa8fc434b
jeraldtsy <jerladtsy@gmail.com>
2020-06-28 12:43:52 +0800

    Learnt about classes and used boardered images

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ import dp from './img/DisplayPicture.jpg';
 function App() {
   return (
     <div className="App">
-        <div class = "image">
-            <img src = {dp} alt = "Display Picture" align = "left" /> 
-            <p>Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain's going to be? He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass.</p>
-            <h1>Jerald </h1>
+        <div class = ".image">
+          <h1>HELLO </h1>
+          <img src = {dp} alt = "Display Picture" className = "image"  /> 
         </div>
     </div>
   );

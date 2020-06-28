e899376e80ac27f2b728860a5a3c1bdec6a52bcb
jeraldtsy <jerladtsy@gmail.com>
2020-06-28 11:35:00 +0800

    created boardered circular image with some text about myself

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import dp from './img/DisplayPicture.jpg';
 
 function App() {
   return (
     <div className="App">
-    <title> jeraldtsy </title>
-      <h1> Jerald </h1>
+        <div class = "image">
+            <img src = {dp} alt = "Display Picture" align = "left" /> 
+            <p>Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain's going to be? He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass.</p>
+            <h1>Jerald </h1>
+        </div>
     </div>
   );
 }
--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import {Route, Switch} from "react-router-dom";
+
 import profileImage from './img/DisplayPicture.jpg'
 import './AboutMe.css';
 
 return (
-	<Image src={profileImage} roundedCircle/>
+	<h1> Jerald </h1>
+	
 );

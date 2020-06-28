f873fb52bbc64a82e1a81a8b3bb93068839d71ba
jeraldtsy <jerladtsy@gmail.com>
2020-06-28 14:17:30 +0800

    Updated header 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+
 import './App.css';
 import dp from './img/DisplayPicture.jpg';
+import {text} from './textfiles/AboutMe.txt';
 
 function App() {
   return (
     <div className="App">
-        <div class = ".image">
-          <h1>HELLO </h1>
-          <img src = {dp} alt = "Display Picture" className = "image"  /> 
+        <div class = ".displayPicture">
+            <img src = {dp} alt = "Display Picture" className = "displayPicture" /> 
+                <div class = ".h1">
+                    <h1>HELLO</h1>
+                </div>
+                <div class = ".AboutMe">
+                    <script class ="AboutMe">console.log(text)</script>
+                </div>
         </div>
     </div>
   );

b1588ec6af1bd5c50527a17a914623f7936b3869
jeraldtsy <jerladtsy@gmail.com>
2020-06-29 11:47:38 +0800

    Inserted links to link to different webpages(For now is google.com

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ function App() {
                     <p className = "AboutMe"> I'm Jerald and I am a christian!<br/>
                     I like to read books and code. <br/><br/>
 
-                    If you would like to read my book reviews(Mostly christian), head over to the "Books" link above. <br/>
-                    If you would like to practise some coding, head over to the "Codes" link above. <br/>
-                    If you would like to read my journal, drop me a request. <br/><br/>
+                    If you would like to read my book reviews(Mostly christian), head over to <a href = "www.google.com">books</a><br/>
+                    If you would like to practise some coding, head over to <a href = "www.google.com">codes</a> <br/>
+                    If you would like to read my <a href = "www.google.com">journal</a>, drop me a request. <br/><br/>
 
                     Corem deo, carpe diem~  </p>
                     </div>

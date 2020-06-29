b19ec25f596e2c868278aaf6187b2d9c199c91a8
jeraldtsy <jerladtsy@gmail.com>
2020-06-29 16:22:30 +0800

    Designed header bar

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ import dp from './img/DisplayPicture.jpg';
 function App() {
   return (
     <div>
-         <div class="header">
-              <h1>link1 link2 link3 </h1>                             
+        <div class="topnav"> 
+             <a href = "https://www.twitch.tv/sheng94">Twitch</a>
+             <a href = "https://www.google.com">Journal</a>
+             <a href = "https://www.google.com">Codes</a>
+             <a href = "https://www.google.com">Books</a>
+             <a href = "https://jeraldtsy.github.io/web/">jeraldtsy</a>
         </div>
         <div class = ".displayPicture">
             <img src = {dp} alt = "dp" className = "displayPicture" /> 
@@ -15,7 +19,7 @@ function App() {
                     <p className = "greet">HELLO</p>               
                 <div class = ".AboutMe">
                     <span className = "AboutMe"> I'm Jerald and I am a christian!<br/>
-                    I like to read books and code. <br/><br/>
+                    I like to read books, code and play <a href = "https://blog.dota2.com/?l=english">DotA</a>. <br/><br/>
 
                     If you would like to read my book reviews(Mostly christian), head over to <a href = "www.google.com">books</a><br/>
                     If you would like to practise some coding, head over to <a href = "www.google.com">codes</a> <br/>
@@ -24,7 +28,13 @@ function App() {
                     Corem deo, carpe diem~  </span>
                     </div>
             </div>
-        </div>     
+        </div>
+        <footer>
+    		<ul>
+        		<li><a href="mailto:jeraldtsy@gmail.com">email</a></li>
+        		<li><a href="https://github.com/jeraldtsy/">github.com/jeraldtsy/</a></li>
+			</ul>
+		</footer>
     </div>  
   );
 }

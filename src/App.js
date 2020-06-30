2134d53d6a5cd026b1470a848d29da1de6c7175d
jeraldtsy <jerladtsy@gmail.com>
2020-06-30 13:44:41 +0800

    Redo bio

--- src/App.js
+++ src/App.js
@@ -34,10 +34,7 @@ function App() {
                     <span className = "AboutMe"> I'm Jerald and I am a christian!<br/>
                     I like to read books, code and play <a href = "https://blog.dota2.com/?l=english">DotA</a>. <br/><br/>
 
-                    If you would like to read my book reviews(Mostly christian), head over to <a href = "www.google.com">books</a><br/>
-                    If you would like to practise some coding, head over to <a href = "www.google.com">codes</a> <br/>
-                    If you would like to read my <a href = "www.google.com">journal</a>, drop me a request. <br/><br/>
-
+                    I firmly believe that tech can be used to share the christian faith and books are a wealth of knowledge that reveals our creator<br/><br/>             
                     Corem deo, carpe diem~  </span>
                     </div>
             </div>

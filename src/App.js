70c1ea078bdf305192df9a583e7f4f42cdc2e762
jeraldtsy <jerladtsy@gmail.com>
2020-06-30 13:25:11 +0800

    Modified footer to have clickable icons instead of words, removed twitch from header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 import './App.css';
 import dp from './img/DisplayPicture.jpg';
+import githubIcon from './img/GithubIcon.jpg';
+import gmailIcon from './img/GmailIcon.jpg';
+import facebookIcon from './img/FacebookIcon.jpg';
+import twitchIcon from './img/TwitchIcon.jpg';
 
 function App() {
     const twitchPage = "https://www.twitch.tv/sheng94";
@@ -11,6 +15,7 @@ function App() {
     const webPage = "https://jeraldtsy.github.io/web/";
     const mailPage = "mailto:jeraldtsy@gmail.com";
     const githubPage = "https://github.com/jeraldtsy";
+    const facebookPage = "https://www.facebook.com/jeraldtsy";
 
   return (
     <div>
@@ -18,8 +23,7 @@ function App() {
              <a href = {webPage}>Jerald Tan</a>
              <a href = {booksPage}>Books</a>
              <a href = {codePage}>Codes</a>                   
-             <a href = {journalPage}>Journal</a>
-             <a href = {twitchPage}>Twitch</a>
+             <a href = {journalPage}>Journal</a>            
         </div>
 
         <div class = ".displayPicture">
@@ -38,12 +42,16 @@ function App() {
                     </div>
             </div>
         </div>  
+
         <div class = ".bibleVerse">
             <span className = "bibleVerse">For the eyes of the Lord range throughout the earth to strengthen those whose hearts are fully committed to him </span>
 		</div>
-        <div class = "footer">           
-            <a href = {mailPage}>email</a>
-        	<a href = {githubPage}>github.com/jeraldtsy/</a>           		    
+
+        <div class = "footer">
+            <a href = {facebookPage}><img src = {facebookIcon} alt = "facebookIcon" className = "iconPictures" /></a>    
+            <a href = {mailPage}><img src = {gmailIcon} alt = "gmailIcon" className = "iconPictures" /></a>
+        	<a href = {githubPage}><img src = {githubIcon} alt = "githubIcon" className = "iconPictures" /></a>
+            <a href = {twitchPage}><img src = {twitchIcon} alt = "twitchIcon" className = "iconPictures" /></a>  
         </div>
     </div>  
   );
